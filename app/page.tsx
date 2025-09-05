3449a5b79b88d08847dba273901eb68b8684e44e
edenWerke <edenw6292@gmail.com>
2025-09-05 15:08:50 +0300

    fetching images from stripe

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,30 @@
 import Image from "next/image";
-import {stripe} from "@/lib/stripe"
-export default async  function Home() {
-  const products=await stripe.products.list({
-  //  expand fetchs data fro staipe with full detail
-    expand:["data.default_price"]
+import { stripe } from "@/lib/stripe";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default async function Home() {
+  const products = await stripe.products.list({
+    // expand fetches data from Stripe with full detail
+    expand: ["data.default_price"],
   });
-  console.log(products)
+  console.log(products);
+
   return (
     <div>
       <section>
         <div>
-            <div>
-          <h2>welcome to estore</h2>
-        <p>Discover Your Favorte fashion</p>
-        </div>
+          <div>
+            <h2>Welcome to eStore</h2>
+            <p>Discover Your Favorite Fashion</p>
+            <Button asChild variant="default">
+              <Link href="/products">Browse All Products</Link>
+            </Button>
+          </div>
+          <Image alt="bannnerimage"
+           width={450} 
+            height={450}
+             src={products.data[1].images[0]}/>
         </div>
       </section>
     </div>
