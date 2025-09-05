d77e8f492ca5fd242b6dfba9e7845b8d69ff15ff
edenWerke <edenw6292@gmail.com>
2025-09-05 14:52:59 +0300

    config stripe

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
 import Image from "next/image";
-
-export default function Home() {
+import {stripe} from "@/lib/stripe"
+export default async  function Home() {
+  const products=await stripe.products.list({
+  //  expand fetchs data fro staipe with full detail
+    expand:["data.default_price"]
+  });
+  console.log(products)
   return (
-    <div></div>
+    <div>
+      <section>
+        <div>
+            <div>
+          <h2>welcome to estore</h2>
+        <p>Discover Your Favorte fashion</p>
+        </div>
+        </div>
+      </section>
+    </div>
   );
 }
