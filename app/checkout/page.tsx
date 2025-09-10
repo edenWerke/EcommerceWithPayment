9090f44795c0ff249730fa6310731c599033ac8e
edenWerke <edenw6292@gmail.com>
2025-09-10 09:54:35 +0300

    header

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,13 +1,30 @@
-'use client'
-import { useCartStore } from "@/store/cart-store"
-export default function ProductPage(){
-    const {items}=useCartStore()
+'use client';
+import { useCartStore } from "@/store/cart-store";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { CardTitle } from "@/components/ui/card";
+export default function ProductPage() {
+  const { items } = useCartStore();
+  
+  const total = items.reduce((acc, item) => acc + item.price * item.quantity, 0);
+  
+  if (total === 0 || items.length === 0) {
+    return <div><h1>Your cart is empty</h1></div>;
+  }
+  
+  return <div><h1>
     
-const total=items.reduce((acc,item)=>acc+ item.price*item.quantity,0)
-   
-if (total===0 || items.length===0){
-    return <div><h1>Your cart is empty</h1></div>
-}
-return 
-     <div>i am from checkout page</div>
+    <Card>
+        <CardHeader>
+
+            <CardTitle>Order Summary</CardTitle>
+        </CardHeader>
+        <CardContent>
+
+            <ul>
+                
+            </ul>
+        </CardContent>
+    </Card>
+    
+    </h1></div>; 
 }
