991d86a6199129d81b1cb842d8e48d05186292c9
edenWerke <edenw6292@gmail.com>
2025-09-10 09:32:31 +0300

    creating checkout

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,3 +1,8 @@
+import { useCartStore } from "@/store/cart-store"
 export default function ProductPage(){
-    return <div>i am from checkout page</div>
+    const {items}=useCartStore()
+    
+const total=items.reduce((acc,item)=>acc+ item.price*item.quantity,0)
+    return 
+     <div>i am from checkout page</div>
 }
