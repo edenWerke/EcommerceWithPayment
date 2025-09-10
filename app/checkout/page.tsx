8b27a4d75e3e1e68a35aca16de3b2b95d98f68ec
edenWerke <edenw6292@gmail.com>
2025-09-10 09:37:26 +0300

    checkout

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,8 +1,13 @@
+'use client'
 import { useCartStore } from "@/store/cart-store"
 export default function ProductPage(){
     const {items}=useCartStore()
     
 const total=items.reduce((acc,item)=>acc+ item.price*item.quantity,0)
-    return 
+   
+if (total===0 || items.length===0){
+    return <div><h1>Your cart is empty</h1></div>
+}
+return 
      <div>i am from checkout page</div>
 }
